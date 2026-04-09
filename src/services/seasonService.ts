54154ef884a5900b03bf533ce01deecc5148c09b
Alan Miranda <alan.miranda@zorbek.com>
2026-04-09 16:58:57 -0700

    Add token to season banner request

--- src/services/seasonService.ts
+++ src/services/seasonService.ts
@@ -1,11 +1,16 @@
 import { requestAxios } from "@/helpers/axiosHelper";
 import { SeasonItemDTO } from "@/models/season-item.dto";
+import { store } from "@/store";
 
 const PATH: string = "seasons";
 
 export async function getSeasonBanner(): Promise<SeasonItemDTO | undefined> {
+    const state = store.getState();
+
     return requestAxios<null, SeasonItemDTO | undefined>(
         "GET",
-        `${PATH}`
+        `${PATH}`,
+        undefined,
+        state.auth.user?.token
     );
 }
